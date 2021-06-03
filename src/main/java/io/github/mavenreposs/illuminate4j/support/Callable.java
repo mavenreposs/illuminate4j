5c22563972399ffa78ca7e820dc04ba93418f750
王正东 <wangzhengdong@ecmoban.com>
2021-06-03 11:54:08 +0800

    增加Callable的get, set新用法

--- src/main/java/io/github/mavenreposs/illuminate4j/support/Callable.java
+++ src/main/java/io/github/mavenreposs/illuminate4j/support/Callable.java
@@ -2,6 +2,8 @@ package io.github.mavenreposs.illuminate4j.support;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Callable<T> {
@@ -14,9 +16,17 @@ public class Callable<T> {
         method.invoke(bean, value);
     }
 
+    public static <T, U, R> void set(T bean, BiConsumer<T, U> fun, U value) throws Exception {
+        fun.accept(bean, value);
+    }
+
     public static <T> Object get(T bean, String fun) throws Exception {
         Method method = bean.getClass().getMethod(fun);
         return method.invoke(bean);
+    }
+
+    public static <T, W> Object get(T bean, Function<T, W> fun) throws Exception {
+        return fun.apply(bean);
     }
 
     protected static  <T> boolean exists(T bean, String fun) throws Exception {
@@ -30,5 +40,9 @@ public class Callable<T> {
 
         return count > 0;
     }
+
+
+
+
 
 }
